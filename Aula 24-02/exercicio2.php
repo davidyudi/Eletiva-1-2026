d1a0f68589e6a5c14c340be0feff6c33e56ce700
David Yudi <davidyudih@gmail.com>
2026-02-24 21:41:34 -0300

    Realizado exercicio 4

--- Aula 24-02/exercicio2.php
+++ Aula 24-02/exercicio2.php
@@ -10,7 +10,7 @@
 
 <body>
     <div class="container py-3">
-        <h1>Exercicio 1</h1>
+        <h1>Exercicio 2</h1>
         <form method="post">
             <div class="mb-3">
                 <label for="valorum" class="form-label">Informe o primeiro valor</label>
