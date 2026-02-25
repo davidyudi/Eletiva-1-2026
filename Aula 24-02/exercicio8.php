a65a24d12a919df732c76f11b995f08758d81a76
David Y <davidyudih@gmail.com>
2026-02-25 11:38:59 -0300

    Exercicio 8 feito

--- Aula 24-02/exercicio8.php
+++ Aula 24-02/exercicio8.php
@@ -4,25 +4,30 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercicio 7</title>
+    <title>Exercicio 8</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="container py-3">
-        <h1>Exercicio 7</h1>
+        <h1>Exercicio 8</h1>
         <form method="post">
             <div class="mb-3">
-                <label for="fahrenheit" class="form-label">Informe a temperatura em Fahrenheit</label>
-                <input type="number" id="fahrenheit" name="fahrenheit" class="form-control" required="">
+                <label for="largura" class="form-label">Informe a largura</label>
+                <input type="number" id="largura" name="largura" class="form-control" required="">
             </div>
-            <button type="submit" class="btn btn-primary">Converter</button>
+            <div class="mb-3">
+                <label for="altura" class="form-label">Informe a altura</label>
+                <input type="number" id="altura" name="altura" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Calcular</button>
         </form>
         <?php
             if($_SERVER['REQUEST_METHOD'] == "POST"){
-                $fahrenheit = $_POST['fahrenheit'];
-                $conv = ($fahrenheit - 32) * 5/9;
-                echo "A conversão resulta em $conv";
+                $largura = $_POST['largura'];
+                $altura = $_POST['altura'];
+                $area = $largura * $altura;
+                echo "A área é $areav";
             }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
