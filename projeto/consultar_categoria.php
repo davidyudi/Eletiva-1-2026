19d54e0fa2a35a722ffa7c3988a2dba233a0e83e
David Yudi <davidyudih@gmail.com>
2026-05-05 21:48:04 -0300

    projeto

--- projeto/consultar_categoria.php
+++ projeto/consultar_categoria.php
@@ -1,5 +1,13 @@
 <?php
     require_once('cabecalho.php');
+    require_once('conexao.php');
+    try {
+        $stmt = $pdo->prepare("SELECT * FROM categoria WHERE id=?");
+        $stmt->execute([$_GET['id']]);
+        $resultado = $stmt->fetch();
+    }   catch (Exception $e) {
+        echo "Erro: ".$e->getMessage();
+    }
 ?>
 
 <h1>Consultar Categoria</h1>
@@ -9,6 +17,21 @@
         </div>
         <button type="submit" class="btn btn-danger">Excluir</button>
     </form>
-
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $id = $_GET['id'];
+            try {
+                $sql = "DELETE FROM categoria WHERE id = ?";
+                $stmt = $pdo->prepare($sql);
+                if($stmt->execute([$id])){
+                    header('Location: categorias.php');
+                } else {
+                    echo "Erro ao excluir!";
+                }
+            }   catch(Exception $e) {
+                echo "Erro: ".$e->getMessage();
+            }
+        }
+    ?>
 <?php
     require_once('rodape.php');
